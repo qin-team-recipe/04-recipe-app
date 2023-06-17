17c4c65f968d6f10df225e4cc18315671af58c72
tekihei2317 <tekihei2317@gmail.com>
2023-06-17 15:43:45 +0900

    シェフの詳細を取得するAPIを作成

---
+++ server/_chef-recipe/get-chef.ts
@@ -0,0 +1,54 @@
+import { publicProcedure } from "../trpc/init-trpc";
+import { notFoundError } from "../trpc/trpc-error";
+import { ChefIdInput } from "./api-schema";
+
+/**
+ * シェフの情報を取得する
+ */
+export const getChef = publicProcedure.input(ChefIdInput).query(async ({ ctx, input }) => {
+  const [chef, recipes] = await Promise.all([
+    ctx.prisma.chef.findUnique({
+      where: { id: input.chefId },
+      select: {
+        id: true,
+        displayName: true,
+        bio: true,
+        links: true,
+        _count: {
+          select: {
+            followers: true,
+          },
+        },
+      },
+    }),
+    ctx.prisma.chefRecipe.findMany({
+      where: { chefId: input.chefId },
+      select: {
+        recipe: {
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            _count: {
+              select: { favorites: true },
+            },
+          },
+        },
+      },
+    }),
+  ]);
+  if (chef === null) throw notFoundError;
+
+  const { _count, ...chefData } = chef;
+
+  return {
+    ...chefData,
+    followerCount: _count.followers,
+    // TODO: ログイン中の場合は、フォローしているかどうかを計算する
+    isFollowing: false,
+    recipes: recipes.map(({ recipe: { _count, ...recipe } }) => ({
+      ...recipe,
+      favoriteCount: _count.favorites,
+    })),
+  };
+});
--- server/_chef-recipe/router.ts
+++ server/_chef-recipe/router.ts
@@ -1,6 +1,8 @@
 import { router } from "../trpc/init-trpc";
+import { getChef } from "./get-chef";
 import { getChefs } from "./get-chefs";
 
 export const chefRecipeRouter = router({
   chefs: getChefs,
+  chef: getChef,
 });
