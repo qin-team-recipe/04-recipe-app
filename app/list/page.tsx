cf9af53d5be6723b67c3cd4fed2094e28f55459e
Natsumi.H <natsmy.1211@gmail.com>
2023-06-22 21:20:18 +0900

    fix prettier

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
-import './styles.css';
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import "./styles.css";
 
 // use clientとmetadata併記するとエラーになる、、
 // export const metadata = {
@@ -11,7 +11,6 @@ import './styles.css';
 // };
 
 // この辺はトグルの挙動確認用なのであとで消して頂いてOKです
-
 
 const myMemoArray = [
   { item: "マカロニ", checked: false },
@@ -55,9 +54,7 @@ export default function List() {
     setGratin(newGratin);
   };
 
-
   return (
-
     <>
       <DropdownMenu.Root>
         {/* ヘッダー */}
@@ -69,9 +66,11 @@ export default function List() {
         <section className=" pt-[8px] pb-[24px] ">
           <div className="flex justify-between py-[12px] items-center px-[16px] border-border border-b-[1px]">
             <h2 className="font-bold text-title text-[16px]">じぶんメモ</h2>
-            <button onClick={() => {
-              window.alert("じぶんメモ追加")
-            }}>
+            <button
+              onClick={() => {
+                window.alert("じぶんメモ追加");
+              }}
+            >
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M10 4.16666V15.8333"
@@ -101,18 +100,24 @@ export default function List() {
                   type="checkbox"
                   className={`
                appearance-none absolute w-[20px] h-[20px] top-1/2 -translate-y-1/2 left-[16px] 
-              ${memo.checked
-                      ? "after:content-[''] after:absolute after:w-[20px] after:h-[20px] bg-[url('/images/checked.png')]"
-                      : "before:content-[''] cursor-pointer before:absolute before:w-[20px] before:h-[20px] before:border-primary before:border-[1.5px] before:rounded-full"
-                    }
+              ${
+                memo.checked
+                  ? "after:content-[''] after:absolute after:w-[20px] after:h-[20px] bg-[url('/images/checked.png')]"
+                  : "before:content-[''] cursor-pointer before:absolute before:w-[20px] before:h-[20px] before:border-primary before:border-[1.5px] before:rounded-full"
+              }
               `}
                   onChange={() => toggleMyMemoChecked(index)}
                   checked={memo.checked}
                 ></input>
                 <label className="text-title">{memo.item}</label>
-                <button className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary" onClick={() => {
-                  window.alert("じぶんメモ削除")
-                }}>削除</button>
+                <button
+                  className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary"
+                  onClick={() => {
+                    window.alert("じぶんメモ削除");
+                  }}
+                >
+                  削除
+                </button>
               </li>
             ))}
           </ul>
@@ -121,7 +126,6 @@ export default function List() {
         {/* レシピ別 */}
 
         <section className=" pt-[8px] pb-[24px]">
-
           <div className="flex justify-between py-[12px] items-center px-[16px] border-border border-b-[1px]">
             <h2 className="font-bold text-title text-[16px]">カレー</h2>
             <DropdownMenu.Trigger>
@@ -156,7 +160,7 @@ export default function List() {
                 </defs>
               </svg>
             </DropdownMenu.Trigger>
-          </div >
+          </div>
 
           <ul className="bg-white">
             {curryArray.map((curry, index) => (
@@ -168,10 +172,11 @@ export default function List() {
                   type="checkbox"
                   className={`
                appearance-none absolute w-[20px] h-[20px] top-1/2 -translate-y-1/2 left-[16px] 
-              ${curry.checked
-                      ? "after:content-[''] after:absolute after:w-[20px] after:h-[20px] bg-[url('/images/checked.png')]"
-                      : "before:content-[''] cursor-pointer before:absolute before:w-[20px] before:h-[20px] before:border-primary before:border-[1.5px] before:rounded-full"
-                    }
+              ${
+                curry.checked
+                  ? "after:content-[''] after:absolute after:w-[20px] after:h-[20px] bg-[url('/images/checked.png')]"
+                  : "before:content-[''] cursor-pointer before:absolute before:w-[20px] before:h-[20px] before:border-primary before:border-[1.5px] before:rounded-full"
+              }
               `}
                   onChange={() => toggleCurryChecked(index)}
                   checked={curry.checked}
@@ -181,19 +186,40 @@ export default function List() {
             ))}
           </ul>
 
-          <DropdownMenu.Portal >
-            <DropdownMenu.Content
-              sideOffset={5}
-              align="end"
-              className="DropdownMenuContent"
-            >
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content sideOffset={5} align="end" className="DropdownMenuContent">
               <ul>
                 <li>
-                  <Link className="py-[6px] pr-[12px] pl-[34px] block text-title hover:bg-backgroundGray relative" href="/recipe/1/steps">
-                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-1/2 -translate-y-1/2 left-[12px]">
-                      <path d="M3 2.75H9L8.25 9.5H3.75L3 2.75Z" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
-                      <path d="M5.25 14H6.75V16.25H5.25V14Z" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
-                      <path d="M14.9999 2.75V11.75H11.2499C11.2327 8.98925 11.3879 6.1955 14.9999 2.75Z" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
+                  <Link
+                    className="py-[6px] pr-[12px] pl-[34px] block text-title hover:bg-backgroundGray relative"
+                    href="/recipe/1/steps"
+                  >
+                    <svg
+                      width="18"
+                      height="19"
+                      viewBox="0 0 18 19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute top-1/2 -translate-y-1/2 left-[12px]"
+                    >
+                      <path
+                        d="M3 2.75H9L8.25 9.5H3.75L3 2.75Z"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M5.25 14H6.75V16.25H5.25V14Z"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M14.9999 2.75V11.75H11.2499C11.2327 8.98925 11.3879 6.1955 14.9999 2.75Z"
+                        stroke="black"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
                       <path d="M15 11.75V16.25H14.25V14" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
                       <path d="M6 9.5V14" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
@@ -202,36 +228,89 @@ export default function List() {
                 </li>
                 <li>
                   <button className="py-[6px] pr-[12px] pl-[34px] text-title block hover:bg-backgroundGray relative w-full text-left">
-                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-1/2 -translate-y-1/2 left-[12px]">
-                      <path d="M4.5 11.75L9 7.25L13.5 11.75" stroke="#1A1523" strokeLinecap="round" strokeLinejoin="round" />
+                    <svg
+                      width="18"
+                      height="19"
+                      viewBox="0 0 18 19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute top-1/2 -translate-y-1/2 left-[12px]"
+                    >
+                      <path
+                        d="M4.5 11.75L9 7.25L13.5 11.75"
+                        stroke="#1A1523"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
                     </svg>
                     上に移動する
                   </button>
                 </li>
                 <li>
                   <button className="py-[6px] pr-[12px] pl-[34px] text-title block hover:bg-backgroundGray relative w-full text-left">
-                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-1/2 -translate-y-1/2 left-[12px]">
-                      <path d="M4.5 7.25L9 11.75L13.5 7.25" stroke="#1A1523" strokeLinecap="round" strokeLinejoin="round" />
+                    <svg
+                      width="18"
+                      height="19"
+                      viewBox="0 0 18 19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute top-1/2 -translate-y-1/2 left-[12px]"
+                    >
+                      <path
+                        d="M4.5 7.25L9 11.75L13.5 7.25"
+                        stroke="#1A1523"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
                     </svg>
                     下に移動する
                   </button>
                 </li>
-                <li >
+                <li>
                   <button className="py-[6px] pr-[12px] pl-[34px] text-title block hover:bg-backgroundGray relative w-full text-left">
-                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-1/2 -translate-y-1/2 left-[12px]">
+                    <svg
+                      width="18"
+                      height="19"
+                      viewBox="0 0 18 19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute top-1/2 -translate-y-1/2 left-[12px]"
+                    >
                       <path d="M9 4.25V14.75" stroke="#1A1523" strokeLinecap="round" strokeLinejoin="round" />
                       <path d="M3.75 9.5H14.25" stroke="#1A1523" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                     買うものを追加する
                   </button>
                 </li>
-                <li >
+                <li>
                   <button className="py-[6px] pr-[12px] pl-[34px] text-primary block hover:bg-backgroundGray relative w-full text-left">
-                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute top-1/2 -translate-y-1/2 left-[12px]">
-                      <path d="M3 14.75C3 15.1478 3.15804 15.5294 3.43934 15.8107C3.72064 16.092 4.10218 16.25 4.5 16.25C4.89782 16.25 5.27936 16.092 5.56066 15.8107C5.84197 15.5294 6 15.1478 6 14.75C6 14.3522 5.84197 13.9706 5.56066 13.6893C5.27936 13.408 4.89782 13.25 4.5 13.25C4.10218 13.25 3.72064 13.408 3.43934 13.6893C3.15804 13.9706 3 14.3522 3 14.75Z" stroke="#E54D2E" strokeLinecap="round" strokeLinejoin="round" />
-                      <path d="M11.25 14.75C11.25 15.1478 11.408 15.5294 11.6893 15.8107C11.9706 16.092 12.3522 16.25 12.75 16.25C13.1478 16.25 13.5294 16.092 13.8107 15.8107C14.092 15.5294 14.25 15.1478 14.25 14.75C14.25 14.3522 14.092 13.9706 13.8107 13.6893C13.5294 13.408 13.1478 13.25 12.75 13.25C12.3522 13.25 11.9706 13.408 11.6893 13.6893C11.408 13.9706 11.25 14.3522 11.25 14.75Z" stroke="#E54D2E" strokeLinecap="round" strokeLinejoin="round" />
+                    <svg
+                      width="18"
+                      height="19"
+                      viewBox="0 0 18 19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute top-1/2 -translate-y-1/2 left-[12px]"
+                    >
+                      <path
+                        d="M3 14.75C3 15.1478 3.15804 15.5294 3.43934 15.8107C3.72064 16.092 4.10218 16.25 4.5 16.25C4.89782 16.25 5.27936 16.092 5.56066 15.8107C5.84197 15.5294 6 15.1478 6 14.75C6 14.3522 5.84197 13.9706 5.56066 13.6893C5.27936 13.408 4.89782 13.25 4.5 13.25C4.10218 13.25 3.72064 13.408 3.43934 13.6893C3.15804 13.9706 3 14.3522 3 14.75Z"
+                        stroke="#E54D2E"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M11.25 14.75C11.25 15.1478 11.408 15.5294 11.6893 15.8107C11.9706 16.092 12.3522 16.25 12.75 16.25C13.1478 16.25 13.5294 16.092 13.8107 15.8107C14.092 15.5294 14.25 15.1478 14.25 14.75C14.25 14.3522 14.092 13.9706 13.8107 13.6893C13.5294 13.408 13.1478 13.25 12.75 13.25C12.3522 13.25 11.9706 13.408 11.6893 13.6893C11.408 13.9706 11.25 14.3522 11.25 14.75Z"
+                        stroke="#E54D2E"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
                       <path d="M12.75 13.25H4.5V2.75H3" stroke="#E54D2E" strokeLinecap="round" strokeLinejoin="round" />
-                      <path d="M4.5 4.25L10.5 4.67825M14.5725 8.00075L14.2508 10.2508H4.50075" stroke="#E54D2E" strokeLinecap="round" strokeLinejoin="round" />
+                      <path
+                        d="M4.5 4.25L10.5 4.67825M14.5725 8.00075L14.2508 10.2508H4.50075"
+                        stroke="#E54D2E"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
                       <path d="M12.75 2.75L15.75 5.75" stroke="#E54D2E" strokeLinecap="round" strokeLinejoin="round" />
                       <path d="M15.75 2.75L12.75 5.75" stroke="#E54D2E" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
@@ -241,14 +320,8 @@ export default function List() {
               </ul>
             </DropdownMenu.Content>
           </DropdownMenu.Portal>
-        </section >
+        </section>
       </DropdownMenu.Root>
-
-
-
-
-
-
     </>
   );
 }
--- app/my-recipe/create/page.tsx
+++ app/my-recipe/create/page.tsx
@@ -10,7 +10,6 @@ import { useState } from "react";
 // };
 
 export default function CreateMyRecipe() {
-
   const [linkArray, setLinkArray] = useState([
     {
       url: "https://www.instagram.com",
@@ -18,55 +17,50 @@ export default function CreateMyRecipe() {
     {
       url: "https://www.chef-website.com",
     },
-  ])
+  ]);
   const addLinkHandler = () => {
-    setLinkArray([...linkArray, { url: "" }])
-  }
+    setLinkArray([...linkArray, { url: "" }]);
+  };
   const deleteLinkHandler = (index: number) => {
-    const newLinkArray = [...linkArray]
-    newLinkArray.splice(index, 1)
-    setLinkArray(newLinkArray)
-  }
+    const newLinkArray = [...linkArray];
+    newLinkArray.splice(index, 1);
+    setLinkArray(newLinkArray);
+  };
 
-
-  const [ingredientArray, setIngredientArray] = useState([
-    "キャベツ 5〜6枚", "チーズ 5〜6枚"])
+  const [ingredientArray, setIngredientArray] = useState(["キャベツ 5〜6枚", "チーズ 5〜6枚"]);
   const addIngredientHandler = () => {
-    setIngredientArray([...ingredientArray, ""])
-  }
+    setIngredientArray([...ingredientArray, ""]);
+  };
   const deleteIngredientHandler = (index: number) => {
-    const newIngredientArray = [...ingredientArray]
-    newIngredientArray.splice(index, 1)
-    setIngredientArray(newIngredientArray)
-  }
+    const newIngredientArray = [...ingredientArray];
+    newIngredientArray.splice(index, 1);
+    setIngredientArray(newIngredientArray);
+  };
 
   const [stepArray, setStepArray] = useState([
     "一行テスト",
-    "複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。"
-  ])
+    "複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。複数行ある場合のダミーテキスト。",
+  ]);
   const addStepHandler = () => {
-    setStepArray([...stepArray, ""])
-  }
+    setStepArray([...stepArray, ""]);
+  };
   const deleteStepHandler = (index: number) => {
-    const newStepArray = [...stepArray]
-    newStepArray.splice(index, 1)
-    setStepArray(newStepArray)
-  }
-
+    const newStepArray = [...stepArray];
+    newStepArray.splice(index, 1);
+    setStepArray(newStepArray);
+  };
 
   const [servings, setServings] = useState(2);
   const servingsIngredientHandler = () => {
     if (servings < 6) {
-      setServings(servings + 1)
+      setServings(servings + 1);
     }
-  }
+  };
   const servingsDecreaseHandler = () => {
     if (servings > 1) {
-      setServings(servings - 1)
+      setServings(servings - 1);
     }
-  }
-
-
+  };
 
   return (
     <>
@@ -92,7 +86,6 @@ export default function CreateMyRecipe() {
         </div>
       </div>
 
-
       {/* レシピ名 */}
       <section className=" pt-[8px] pb-[24px] ">
         <label className="flex justify-between py-[12px] items-center px-[16px] border-border border-b-[1px]">
@@ -109,25 +102,30 @@ export default function CreateMyRecipe() {
         <div className="flex gap-x-[20px] py-[12px] items-center px-[16px] border-border border-b-[1px]">
           <h2 className="font-bold text-title text-[16px]">材料 / {servings}人前</h2>
           <div className="flex gap-x-[10px] text-[25px]">
-            <button className={servings > 1 ? "text-primary" : ""} onClick={servingsDecreaseHandler}>-</button>
-            <button className={servings < 6 ? "text-primary" : ""} onClick={servingsIngredientHandler}>+</button>
+            <button className={servings > 1 ? "text-primary" : ""} onClick={servingsDecreaseHandler}>
+              -
+            </button>
+            <button className={servings < 6 ? "text-primary" : ""} onClick={servingsIngredientHandler}>
+              +
+            </button>
           </div>
         </div>
 
         <ul className="bg-white">
-          {
-            ingredientArray.map((ingredient, index) => (
-              <li className=" text-[14px] border-border border-b-[1px] leading-[18px] relative" key={index}>
-                <input className="bg-white w-full  py-[13px] px-[16px] text-title focus:outline-text" defaultValue={ingredient}>
-                </input>
-                <button className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary" onClick={
-                  () => deleteIngredientHandler(index)
-                }>削除</button>
-              </li>
-            ))
-          }
-
-
+          {ingredientArray.map((ingredient, index) => (
+            <li className=" text-[14px] border-border border-b-[1px] leading-[18px] relative" key={index}>
+              <input
+                className="bg-white w-full  py-[13px] px-[16px] text-title focus:outline-text"
+                defaultValue={ingredient}
+              ></input>
+              <button
+                className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary"
+                onClick={() => deleteIngredientHandler(index)}
+              >
+                削除
+              </button>
+            </li>
+          ))}
         </ul>
         <div className="py-[12px] px-[16px] ">
           <button className="text-primary relative pl-[20px]" onClick={addIngredientHandler}>
@@ -147,48 +145,51 @@ export default function CreateMyRecipe() {
             材料を追加する
           </button>
         </div>
-      </section >
+      </section>
 
       {/* 作り方 */}
-      <section className=" pt-[8px] pb-[24px] " >
+      <section className=" pt-[8px] pb-[24px] ">
         <div className="flex justify-between py-[12px] items-center px-[16px] border-border border-b-[1px]">
           <h2 className="font-bold text-title text-[16px]">作り方</h2>
         </div>
 
         <ul className="bg-white">
-          {
-            stepArray.map((step, index) => (
-              <li className="text-title text-[14px] py-[13px] pl-[42px] pr-[50px] border-border border-b-[1px] bg-white leading-[18px] relative" key={index}>
-                <p className="block" >
-                  {step}
-                </p>
-                <button className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary" onClick={
-                  () => deleteStepHandler(index)
-                }>削除</button>
-                <span className="absolute top-[14px] left-[16px] w-[18px] h-[18px] text-white text-[12px] rounded-full bg-primary text-center leading-[18px] ">
-                  {index + 1}
-                </span>
-              </li>
-              // <li className="relative" key={index}>
-              //   <input className="block py-[8px] pr-[16px] pl-[42px] border-b-[1px] border-border w-full" defaultValue={step}>
-              //     {/* <p className="text-title text-[14px]">
-              //   用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。
-              // </p> */}
-              //     {/* <p className="mt-[4px] text-[10px]">
-              //   ※椎茸はなしでも作れますし、しめじやマッシュルームなどでも。きのこ系が入っていた方が食感と香りがよいので、ぜひ入れて作ってみてください。鶏肉等の代用については下記補足に
-              // </p> */}
-              //   </input>
-              //   <span className="absolute top-[8px] left-[16px] w-[18px] h-[18px] text-white text-[12px] rounded-full bg-primary text-center leading-[18px] ">
-              //     1
-              //   </span>
-              // </li>
-            ))}
-
+          {stepArray.map((step, index) => (
+            <li
+              className="text-title text-[14px] py-[13px] pl-[42px] pr-[50px] border-border border-b-[1px] bg-white leading-[18px] relative"
+              key={index}
+            >
+              <p className="block">{step}</p>
+              <button
+                className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary"
+                onClick={() => deleteStepHandler(index)}
+              >
+                削除
+              </button>
+              <span className="absolute top-[14px] left-[16px] w-[18px] h-[18px] text-white text-[12px] rounded-full bg-primary text-center leading-[18px] ">
+                {index + 1}
+              </span>
+            </li>
+            // <li className="relative" key={index}>
+            //   <input className="block py-[8px] pr-[16px] pl-[42px] border-b-[1px] border-border w-full" defaultValue={step}>
+            //     {/* <p className="text-title text-[14px]">
+            //   用意するメインの材料は、マカロニ、牛乳、鶏もも肉、玉ねぎ、椎茸で、バター、小麦粉、塩、こしょうも使用します。
+            // </p> */}
+            //     {/* <p className="mt-[4px] text-[10px]">
+            //   ※椎茸はなしでも作れますし、しめじやマッシュルームなどでも。きのこ系が入っていた方が食感と香りがよいので、ぜひ入れて作ってみてください。鶏肉等の代用については下記補足に
+            // </p> */}
+            //   </input>
+            //   <span className="absolute top-[8px] left-[16px] w-[18px] h-[18px] text-white text-[12px] rounded-full bg-primary text-center leading-[18px] ">
+            //     1
+            //   </span>
+            // </li>
+          ))}
         </ul>
         <div className="py-[12px] px-[16px] ">
-          <button className="text-primary relative pl-[20px]"
+          <button
+            className="text-primary relative pl-[20px]"
             onClick={() => {
-              window.alert("工程を追加します")
+              window.alert("工程を追加します");
             }}
           >
             <span className="absolute top-1/2 -translate-y-1/2 left-0 w-[16px] h-[16px]">
@@ -207,10 +208,10 @@ export default function CreateMyRecipe() {
             工程を追加する
           </button>
         </div>
-      </section >
+      </section>
 
       {/* 画像 */}
-      <section className="pt-[8px] pb-[24px]  overflow-x-hidden" >
+      <section className="pt-[8px] pb-[24px]  overflow-x-hidden">
         <div className="flex justify-between py-[12px] items-center px-[16px] ">
           <h2 className="font-bold text-title text-[16px]">画像</h2>
         </div>
@@ -218,8 +219,12 @@ export default function CreateMyRecipe() {
         <div className="px-[16px]">
           <div className="inline-block relative">
             <Image src="/images/top/recipes/recipe1.png" alt="レシピの写真" width={128} height={128} />
-            <button type="button" className="bg-primary absolute rounded-full w-[20px] h-[20px] -top-[5px] -right-[5px]">
-              <svg xmlns="http://www.w3.org/2000/svg"
+            <button
+              type="button"
+              className="bg-primary absolute rounded-full w-[20px] h-[20px] -top-[5px] -right-[5px]"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
                 className="stroke-white ml-[4px]"
                 width="12"
                 height="12"
@@ -235,14 +240,13 @@ export default function CreateMyRecipe() {
               </svg>
             </button>
           </div>
-
-
         </div>
 
         <div className="py-[12px] px-[16px] ">
-          <button className="text-primary relative pl-[20px]"
+          <button
+            className="text-primary relative pl-[20px]"
             onClick={() => {
-              window.alert("画像を追加します")
+              window.alert("画像を追加します");
             }}
           >
             <span className="absolute top-1/2 -translate-y-1/2 left-0 w-[16px] h-[16px]">
@@ -264,30 +268,30 @@ export default function CreateMyRecipe() {
       </section>
 
       {/* リンク */}
-      <section className=" pt-[8px] pb-[24px] " >
+      <section className=" pt-[8px] pb-[24px] ">
         <div className="flex justify-between py-[12px] items-center px-[16px] border-border border-b-[1px]">
           <h2 className="font-bold text-title text-[16px]">リンク</h2>
         </div>
 
         <ul className="bg-white">
-          {
-            linkArray.map((link, index) => (
-              <li className=" text-[14px] border-border border-b-[1px] leading-[18px] relative" key={index}>
-                <input
-                  className="py-[15.5px] px-[16px] block text-title text-[14px] w-full focus:outline-text"
-                  defaultValue={link.url}
-                ></input>
-                <button className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary" onClick={() => deleteLinkHandler(index)}>削除</button>
-              </li>
-            )
-            )
-          }
-        </ul >
+          {linkArray.map((link, index) => (
+            <li className=" text-[14px] border-border border-b-[1px] leading-[18px] relative" key={index}>
+              <input
+                className="py-[15.5px] px-[16px] block text-title text-[14px] w-full focus:outline-text"
+                defaultValue={link.url}
+              ></input>
+              <button
+                className="absolute top-1/2 -translate-y-1/2 right-[16px] text-primary"
+                onClick={() => deleteLinkHandler(index)}
+              >
+                削除
+              </button>
+            </li>
+          ))}
+        </ul>
 
         <div className="py-[12px] px-[16px] ">
-          <button className="text-primary relative pl-[20px]"
-            onClick={addLinkHandler}
-          >
+          <button className="text-primary relative pl-[20px]" onClick={addLinkHandler}>
             <span className="absolute top-1/2 -translate-y-1/2 left-0 w-[16px] h-[16px]">
               <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clipPath="url(#clip0_391_2064)">
@@ -304,8 +308,7 @@ export default function CreateMyRecipe() {
             リンクを追加する
           </button>
         </div>
-      </section >
-
+      </section>
     </>
   );
 }
