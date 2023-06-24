2716344ba3a751c4dcd5fbbfacc08987ab7b1ed2
kimura706 <kimura.r706@gmail.com>
2023-06-24 21:58:56 +0900

    レシピ一覧取得APIの実装

---
+++ server/_chef-recipe/get-recipes.ts
@@ -0,0 +1,20 @@
+import Favorite from "@/app/favorite/page";
+import { publicProcedure } from "../trpc/init-trpc";
+
+export const getRecipes = publicProcedure.query(async ({ ctx }) => {
+  const recipes = await ctx.prisma.recipe.findMany({
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      images: true,
+      _count: { select: { favorites: true } },
+    },
+    where: {
+      chefRecipe: {
+        isNot: null,
+      },
+    },
+  });
+  return recipes;
+});
--- server/_chef-recipe/router.ts
+++ server/_chef-recipe/router.ts
@@ -1,6 +1,8 @@
 import { router } from "../trpc/init-trpc";
 import { getChefs } from "./get-chefs";
+import { getRecipes } from "./get-recipes";
 
 export const chefRecipeRouter = router({
   chefs: getChefs,
+  recipes: getRecipes,
 });
