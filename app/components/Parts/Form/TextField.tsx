643f84d513b0f6aa769f63f7bc0196aff3ba8dac
mu0363 <dde036@gmail.com>
2023-07-10 16:19:49 +0900

    変数名を変更

--- app/components/Parts/Form/TextField.tsx
+++ app/components/Parts/Form/TextField.tsx
@@ -1,15 +1,17 @@
+import { useFormContext } from "react-hook-form";
 import type { UseFormRegister, FormState, FieldValues, Path } from "react-hook-form";
 
 type Props<T extends FieldValues> = {
   label: string;
-  registerName: Path<T>;
-  register: UseFormRegister<T>;
-  formState: FormState<T>;
+  fieldName: Path<T>;
   multiline?: boolean;
 };
 
-export function TextField<T extends FieldValues>({ label, registerName, register, formState, multiline }: Props<T>) {
-  const { errors } = formState;
+export function TextField<T extends FieldValues>({ label, fieldName, multiline }: Props<T>) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<T>();
   const commonStyle =
     "border-t-[1px] border-b-[1px] border-border py-[9px] px-4 w-full focus:outline-none resize-none text-black";
 
@@ -17,11 +19,11 @@ export function TextField<T extends FieldValues>({ label, registerName, register
     <div>
       <label className="font-bold text-title mb-1 px-4">{label}</label>
       {multiline ? (
-        <textarea rows={4} className={`${commonStyle} resize-none`} {...register(registerName)} />
+        <textarea rows={4} className={`${commonStyle} resize-none`} {...register(fieldName)} />
       ) : (
-        <input type="text" className={commonStyle} {...register(registerName)} />
+        <input type="text" className={commonStyle} {...register(fieldName)} />
       )}
-      {errors && <p className="text-primary px-4 font-bold text-sm">{errors[registerName]?.message as string}</p>}
+      {errors && <p className="text-primary px-4 font-bold text-sm">{errors[fieldName]?.message as string}</p>}
     </div>
   );
 }
