c19b8db6d969a7f9f4552b7a2a20ee2a4f4eba56
tekihei2317 <tekihei2317@gmail.com>
2023-07-23 15:27:36 +0900

    設定画面のマークアップ

--- app/settings/LogoutButton.tsx
+++ app/settings/LogoutButton.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { signOut } from "next-auth/react";
+import { Logout } from "tabler-icons-react";
 
 export const LogoutButton = () => {
-  return <button onClick={() => signOut()}>ログアウト</button>;
+  return (
+    <button onClick={() => signOut()} className="px-4 py-3 flex justify-between w-full">
+      ログアウト
+      <Logout />
+    </button>
+  );
 };
