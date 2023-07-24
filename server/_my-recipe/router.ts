261dc737c956aab9c2fe2f1950d93248bb5fbb31
kimura706 <kimura.r706@gmail.com>
2023-07-25 06:14:17 +0900

    マイレシピ登録用ルート実装

--- server/_my-recipe/router.ts
+++ server/_my-recipe/router.ts
@@ -1,6 +1,8 @@
 import { router } from "../trpc/init-trpc";
 import { getMyRecipes } from "./get-myRecipes";
+import { registrationMyRecipe } from "./reg-myRecipe";
 
 export const myRecipeRouter = router({
   myrecipes: getMyRecipes,
+  regmyrecipe: registrationMyRecipe,
 });
