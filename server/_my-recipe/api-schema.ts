92e6d83f27935ac66b09c9ec98d80403b51d0889
kimura706 <kimura.r706@gmail.com>
2023-07-25 06:29:17 +0900

    登録データ受け取り用zodオブジェクトを作成

--- server/_my-recipe/api-schema.ts
+++ server/_my-recipe/api-schema.ts
@@ -3,3 +3,14 @@ import { z } from "zod";
 export const UserIdInput = z.object({
   userId: z.string(),
 });
+
+export const RegistrationMyRecipeInput = z.object({
+  //TODO文字数チェックを追加する
+  name: z.string(),
+  ingredients: z.array(z.string()),
+  yields: z.number().max(6).min(1),
+  processes: z.array(z.string()),
+  images: z.array(z.string()),
+  description: z.string().optional(),
+  urls: z.array(z.string().url()),
+});
