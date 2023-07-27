b8ffaa822343506af99f907853c142df4cd51207
kimura706 <kimura.r706@gmail.com>
2023-07-28 06:49:45 +0900

    base64から画像への変換処理を追加

--- server/_my-recipe/reg-myRecipe.ts
+++ server/_my-recipe/reg-myRecipe.ts
@@ -1,6 +1,7 @@
 import { TRPCError } from "@trpc/server";
 import { protectedProcedure } from "../trpc/init-trpc";
 import { RegistrationMyRecipeInput } from "./api-schema";
+import { base64ToImage } from "./utils";
 
 /**
  * マイレシピを新規登録する
@@ -9,7 +10,17 @@ export const registrationMyRecipe = protectedProcedure
   .input(RegistrationMyRecipeInput)
   .mutation(async ({ ctx, input }) => {
     //TODObase64→画像への変換処理
+    let imageNum = 1;
+    for (const image of input.images) {
+      // Base64データ
+      const base64Data = image;
+      // 画像に変換してファイルに保存
+      imageNum++;
+      base64ToImage(base64Data, `./images/image_${imageNum}`);
+    }
+
     //TODOCloudinaryへの登録処理
+
     // const imageUrls[]=["http://image1","http://image3","http://image3"]
     //TODO渡されたＪＳＯＮをもとにrecipeテーブルとマイレシピテーブルと、画像、材料、作り方、リンクに登録処理？
     // return await ctx.prisma.recipe.create({
