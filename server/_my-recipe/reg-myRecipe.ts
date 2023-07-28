ec836721b88068d9771a6b319591d9fc793ca949
kimura706 <kimura.r706@gmail.com>
2023-07-28 21:00:57 +0900

    Cloudinary登録処理

--- server/_my-recipe/reg-myRecipe.ts
+++ server/_my-recipe/reg-myRecipe.ts
@@ -2,6 +2,9 @@ import { TRPCError } from "@trpc/server";
 import { protectedProcedure } from "../trpc/init-trpc";
 import { RegistrationMyRecipeInput } from "./api-schema";
 import { base64ToImage } from "./utils";
+import * as cloudinary from "cloudinary";
+import * as path from "path";
+import * as fs from "fs";
 
 /**
  * マイレシピを新規登録する
@@ -21,33 +24,67 @@ export const registrationMyRecipe = protectedProcedure
 
     //TODOCloudinaryへの登録処理
 
-    // const imageUrls[]=["http://image1","http://image3","http://image3"]
+    // Cloudinaryの設定
+    cloudinary.v2.config({
+        cloud_name: 'dmvffahda', 
+        api_key: '964782862971153', 
+        api_secret: 'ERErOlKWsroJMaMYF22bQ6S485I' 
+    });
+
+    async function uploadImageToCloudinary(localImagePath: string) {
+      try {
+        const result = await cloudinary.v2.uploader.upload(localImagePath, {
+          folder: "./iamges", 
+        });
+
+        console.log("公開URL:", result.secure_url);
+      } catch (error) {
+        console.error("エラー", error);
+      }
+    }
+
+    // ローカルの画像フォルダパス
+    const localImageFolderPath = path.join(__dirname, "path/to/your/local/folder");
+
+    fs.readdir(localImageFolderPath, (err, files) => {
+      if (err) {
+        console.error("フォルダの読み込み中にエラーが発生しました。", err);
+        return;
+      }
+
+      files.forEach((file) => {
+        const localImagePath = path.join(localImageFolderPath, file);
+        uploadImageToCloudinary(localImagePath);
+      });
+    });
+
+    const imageUrls[]=["http://image1","http://image3","http://image3"]
     //TODO渡されたＪＳＯＮをもとにrecipeテーブルとマイレシピテーブルと、画像、材料、作り方、リンクに登録処理？
-    // return await ctx.prisma.recipe.create({
-    //     data: {
-    //         name: input.name,
-    //         ingredients:{
-    //             createMany:{
-    //                 data:[{title:"test"}]
-    //             }
-    //         },
-    //         yields:input.yields,
-    //         processes:{
-    //             createMany:{
-    //                 data:[{title:"test"}]
-    //             }
-    //         },
-    //         images:{
-    //             createMany:{
-    //                 data:[imageUrls]
-    //             }
-    //         },
-    //         description:input.description,
-    //         links:{
-    //             createMany:{
-    //                 data:[{title:"test"}]
-    //             }
-    //         },
-    //     },
-    // });
+    return await ctx.prisma.recipe.create({
+        data: {
+            name: input.name,
+            ingredients:{
+                createMany:{
+                    data:[{title:"test"}]
+                }
+            },
+            yields:input.yields,
+            processes:{
+                createMany:{
+                    data:[{title:"test"}]
+                }
+            },
+            images:{
+                createMany:{
+                    data:[imageUrls]
+                }
+            },
+            description:input.description,
+            links:{
+                createMany:{
+                    data:[{title:"test"}]
+                }
+            },
+        },
+    });
   });
