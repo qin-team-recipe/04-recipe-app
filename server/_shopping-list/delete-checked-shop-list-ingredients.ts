b17c89f0d4c7c115d2c85cf5f3562dd3427993c3
tekihei2317 <tekihei2317@gmail.com>
2023-08-07 11:27:22 +0900

    買い物リストのレシピを削除するAPIと、チェックした材料を削除するAPIを削除

--- server/_shopping-list/delete-checked-shop-list-ingredients.ts
+++ server/_shopping-list/delete-checked-shop-list-ingredients.ts
@@ -1,4 +1,5 @@
 import { protectedProcedure } from "../trpc/init-trpc";
+import { notFoundError } from "../trpc/trpc-error";
 import { ShopListRecipeIdInput } from "./api-schema";
 
 /**
@@ -6,4 +7,20 @@ import { ShopListRecipeIdInput } from "./api-schema";
  */
 export const deleteCheckedShopListIngredients = protectedProcedure
   .input(ShopListRecipeIdInput)
-  .mutation(async () => {});
+  .mutation(async ({ ctx, input }) => {
+    const shopListRecipe = await ctx.prisma.shopListRecipe.findUnique({ where: { id: input.shopListRecipeId } });
+    if (shopListRecipe === null) {
+      throw notFoundError;
+    }
+    if (shopListRecipe.userId !== ctx.user.userId) {
+      throw notFoundError;
+    }
+
+    const result = await ctx.prisma.shopListIngredient.deleteMany({
+      where: {
+        shopListRecipeId: shopListRecipe.id,
+        isChecked: true,
+      },
+    });
+    return { deletedCount: result.count };
+  });
--- server/_shopping-list/delete-shop-list-recipe.ts
+++ server/_shopping-list/delete-shop-list-recipe.ts
@@ -1,7 +1,27 @@
 import { protectedProcedure } from "../trpc/init-trpc";
+import { notFoundError } from "../trpc/trpc-error";
 import { ShopListRecipeIdInput } from "./api-schema";
 
 /**
  * 買い物リストのレシピを削除する
  */
-export const deleteShopListRecipe = protectedProcedure.input(ShopListRecipeIdInput).mutation(async () => {});
+export const deleteShopListRecipe = protectedProcedure.input(ShopListRecipeIdInput).mutation(async ({ ctx, input }) => {
+  const shopListRecipe = await ctx.prisma.shopListRecipe.findUnique({
+    where: { id: input.shopListRecipeId },
+  });
+  if (shopListRecipe === null) {
+    throw notFoundError;
+  }
+  if (shopListRecipe.userId !== ctx.user.userId) {
+    throw notFoundError;
+  }
+
+  await ctx.prisma.$transaction(async (tx) => {
+    // 買い物リストのレシピの材料を削除する
+    await tx.shopListIngredient.deleteMany({
+      where: { shopListRecipeId: input.shopListRecipeId },
+    });
+    // 買い物リストのレシピを削除する
+    await tx.shopListRecipe.delete({ where: { id: input.shopListRecipeId } });
+  });
+});
