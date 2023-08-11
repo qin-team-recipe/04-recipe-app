e6c0defae8bd7ae355c3253fc96b3044e62c3399
kimura706 <kimura.r706@gmail.com>
2023-08-11 18:12:41 +0900

    関数名の変更

--- server/_my-recipe/router.ts
+++ server/_my-recipe/router.ts
@@ -1,8 +1,8 @@
 import { router } from "../trpc/init-trpc";
 import { getMyRecipes } from "./get-myRecipes";
-import { registrationMyRecipe } from "./create-my-recipe";
+import { createMyRecipe } from "./create-my-recipe";
 
 export const myRecipeRouter = router({
   myrecipes: getMyRecipes,
-  regmyrecipe: registrationMyRecipe,
+  regmyrecipe: createMyRecipe,
 });
