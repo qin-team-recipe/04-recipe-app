bc24dee18e667b3b6bf0b5311e4914c805dc9f27
Natsumi.H <natsmy.1211@gmail.com>
2023-08-28 21:13:42 +0800

    CreateMyRecipeButtonのスタイル変更

--- app/components/MyPage/CreateMyRecipeButton.tsx
+++ app/components/MyPage/CreateMyRecipeButton.tsx
@@ -4,7 +4,7 @@ export default function CreateMyRecipeButton() {
   return (
     <Link
       href="/my-recipe/create"
-      className="text-white bg-primary py-[8px] px-[12px] inline-block rounded-full fixed left-1/2 -translate-x-1/2 bottom-[81px] md:bottom-[24px]"
+      className="text-white bg-primary py-[14.5px] px-[20px] inline-block rounded-full fixed left-1/2 -translate-x-1/2 bottom-[81px] md:bottom-[24px] font-bold leading-[19px] shadow-md"
     >
       マイレシピを追加する
     </Link>
