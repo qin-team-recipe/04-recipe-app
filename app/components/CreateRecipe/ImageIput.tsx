f0d722e2a02d82f2e5ed6c3fa15ca7dfad2fd41f
yanachuwan9sm <gurem919@icloud.com>
2023-09-08 20:12:01 +0900

    fix: preview画像はuseStateで管理するように修正

--- app/components/CreateRecipe/ImageIput.tsx
+++ app/components/CreateRecipe/ImageIput.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useFormContext, useWatch } from "react-hook-form";
 import { ValidationError } from "./Parts/ValidationError";
+import { useState } from "react";
 
 export default function ImageInput() {
   const {
@@ -14,16 +15,23 @@ export default function ImageInput() {
     name: "image",
   });
 
+  const [previewImageData, setPreviewImageData] = useState<string>("");
+
   const uploadImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    const file = files![0];
+
     const reader = new FileReader();
     reader.onload = () => {
-      setValue("image", reader.result as string);
+      setValue("image", files);
+      setPreviewImageData(reader.result as string);
     };
-    reader.readAsDataURL(e.target.files![0]);
+    reader.readAsDataURL(file);
   };
 
   const deleteImageHandler = () => {
-    setValue("image", "");
+    setValue("image", null);
+    setPreviewImageData("");
   };
 
   const { onBlur, name, ref } = register("image");
@@ -38,7 +46,7 @@ export default function ImageInput() {
         <div className="inline-block relative">
           {image ? (
             <>
-              <Image src={image} alt="レシピの写真" width={128} height={128} />
+              <Image src={previewImageData} alt="レシピの写真" width={128} height={128} />
               <button
                 type="button"
                 className="bg-primary absolute rounded-full w-[20px] h-[20px] -top-[5px] -right-[5px]"
