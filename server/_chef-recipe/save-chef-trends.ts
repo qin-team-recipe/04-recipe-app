49923e1a43013981eb030b312662a499c573b2b1
tekihei2317 <tekihei2317@gmail.com>
2023-09-13 22:07:32 +0900

    注目のシェフの計算処理を実装

--- server/_chef-recipe/save-chef-trends.ts
+++ server/_chef-recipe/save-chef-trends.ts
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+import { prisma } from "../database/prisma";
+
 type ChefTrend = {
   chefId: string;
   /** 獲得フォロワー数 */
@@ -7,13 +10,39 @@ type ChefTrend = {
 /**
  * 注目のシェフを計算する。注目のシェフは、直近3日間の獲得フォロワー数の上位10人。
  */
-async function calculateFeaturedChefs(): Promise<ChefTrend[]> {
-  return [];
+async function calculateChefTrends(prisma: PrismaClient): Promise<ChefTrend[]> {
+  const chefs = await prisma.$queryRaw<ChefTrend[]>`
+    select
+      chefId,
+      count(chefId) as newFollowers
+    from
+      Following
+    -- TODO: 直近3日間での絞り込み
+    group by
+      chefId
+    order by
+      newFollowers desc
+    limit 10;
+  `;
+
+  return chefs;
 }
 
 /**
  * 注目のシェフを計算して、データベースに保存する
  */
 export async function saveChefTrends(): Promise<void> {
-  const chefs = await calculateFeaturedChefs();
+  const chefTrends = await calculateChefTrends(prisma);
+
+  // 運用するまではトレンドの履歴は不要のため、最新のトレンドのみ保存する
+  await prisma.$transaction(async (tx) => {
+    await tx.chefTrend.deleteMany();
+
+    await tx.chefTrend.createMany({
+      data: chefTrends.map((trend) => ({
+        chefId: trend.chefId,
+        newFollowers: trend.newFollowers,
+      })),
+    });
+  });
 }
